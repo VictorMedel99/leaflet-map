5e23ea8d051acb0669854bcfaf18c94dea6cf5fb
VictorMedel99 <victor.medelito@gmail.com>
2020-09-29 16:17:22 -0500

    se añadio el popup service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { MarkerService } from './_services/marker.service';
+import { HttpClientModule } from '@angular/common/http';
+import { PopUpService } from './_services/pop-up.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MarkerService, PopUpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
